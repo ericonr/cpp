31893ba3a032bb864158ed31d1d84946830bd96d
Érico Nogueira <erico.erc@gmail.com>
2021-09-02 15:26:27 -0300

    Começar a adicionar SDL2 a mma.
    - ainda não inclui entrada ou loop de controle para permitir saída

--- massa-mola-amortecedor/mma.cc
+++ massa-mola-amortecedor/mma.cc
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <memory>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 // structs para proibir operações entre valores incompatíveis
 struct Position {
@@ -63,16 +68,58 @@ class Amortecedor {
 };
 
 class View {
-	std::ostream &out;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Texture *texture, *bg;
+	SDL_Rect target;
+
 	public:
-		View(std::ostream &out): out(out) {}
+		View():
+			window(nullptr), renderer(nullptr),
+			texture(nullptr), bg(nullptr)
+		{
+			if (SDL_Init (SDL_INIT_VIDEO) < 0) {
+				throw std::runtime_error(SDL_GetError());
+			}
+			window = SDL_CreateWindow("Massa mola",
+					SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+					SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+			if (window == nullptr) {
+				SDL_Quit();
+				throw std::runtime_error(SDL_GetError());
+			}
+			renderer = SDL_CreateRenderer(window, -1,
+					SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+			if (renderer == nullptr) {
+				SDL_DestroyWindow(window);
+				SDL_Quit();
+				throw std::runtime_error(SDL_GetError());
+			}
+
+			texture = IMG_LoadTexture(renderer, "../sdl/capi.png");
+			bg = IMG_LoadTexture(renderer, "../sdl/park.jpeg");
+			target.x = 0;
+			target.y = 0;
+			SDL_QueryTexture(texture, nullptr, nullptr, &target.w, &target.h);
+		}
+		void begin_print() { SDL_RenderClear(renderer); }
 		void print(const Massa &m);
 		void print(const Time &t);
-		void finish_print() { out << std::endl; }
+		void finish_print() { std::cout << std::endl; SDL_RenderPresent(renderer); }
 };
 
-void View::print(const Massa &m) { out << m.x.x << "," << m.xd.x; }
-void View::print(const Time &t) { out << t.t << ","; }
+void View::print(const Massa &m)
+{
+	std::cout << m.x.x << "," << m.xd.x;
+
+	SDL_RenderCopy(renderer, bg, nullptr, nullptr);
+	target.x = m.x.x;
+	SDL_RenderCopy(renderer, texture, nullptr, &target);
+}
+void View::print(const Time &t)
+{
+	std::cout << t.t << ",";
+}
 
 // sistema implementando um modelo massa-mola-amortecedor
 // as equações utilizadas para o modelo foram:
@@ -120,7 +167,7 @@ class Sistema {
 
 int main()
 {
-	View v{std::cout};
+	View v{};
 	// m = 10kg
 	// k = 3 N/m
 	// b = 1.5 kg/s
